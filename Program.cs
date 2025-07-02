f5dd48af02401b1e0e54fc5dc3166d0db826d4eb
fernandesnic <nicolasbafernandes@gmail.com>
2025-07-02 11:23:43 -0300

    implementação do codigo exigido no desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "11999999999", modelo: "Nokia 3310", imei: "1234567890", 64);
+nokia.InstalarAplicativo(nomeApp: "Snake");
+nokia.Ligar();
+Smartphone iphone = new Iphone(numero: "11988888888", modelo: "iPhone 12", imei: "0987654321", 128);
+iphone.InstalarAplicativo(nomeApp: "iMessage");
+iphone.Ligar();
